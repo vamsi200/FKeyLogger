c3f4e774d86e721cf47f88241fa715534181a2eb
vamsi200 <vamsivanama891@gmail.com>
2025-05-12 20:44:50 +0530

    updated bpf file

--- src/test.bpf.c
+++ src/test.bpf.c
@@ -6,29 +6,58 @@
 
 char LICENSE[] SEC("license") = "GPL";
 
-//ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
+#define MAJOR(dev) ((unsigned int)((dev) >> 20))
+#define MINOR(dev) ((unsigned int)((dev) & 0xfffff))
+
+struct event_t {
+	u32 pid;
+	u32 major;
+	u32 minor;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(key_size, sizeof(u32));
+	__uint(value_size, sizeof(u8));
+	__uint(max_entries, 10240);
+} seen_pids SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(key_size, sizeof(u32));
+	__uint(value_size, sizeof(u32));
+	__uint(max_entries, 1024);
+} events SEC(".maps");
+
 SEC("kprobe/vfs_read")
 int handle_vfs_read(struct pt_regs *ctx)
 {
-	//PT_REGS_PARM1 will get the first argument passed to vfs_read() that is a file struct, essentially meaning, get the file that is being read.
 	struct file *file = (struct file *)PT_REGS_PARM1(ctx);
-	struct inode *inode;
-	dev_t dev;
-
 	if (!file)
 		return 0;
 
-	inode = BPF_CORE_READ(file, f_inode);
-	// this will get the device id, which contains the major and minor numbers
-	dev = BPF_CORE_READ(inode, i_rdev);
+	struct inode *inode = BPF_CORE_READ(file, f_inode);
+	dev_t dev = BPF_CORE_READ(inode, i_rdev);
 
-	unsigned int major = dev >> 20;
-	unsigned int minor = dev & 0xFFF;
-	// major 13 is /dev/input and minor 64-95 are event1, event2..
+	unsigned int major = MAJOR(dev);
+	unsigned int minor = MINOR(dev);
+
 	if (major == 13 && minor >= 64 && minor <= 95) {
-		int pid = bpf_get_current_pid_tgid() >> 32;
-		bpf_printk("PID %d read from /dev/input/event* device\n and event - %d", pid,
-			   minor);
+		u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+		u8 dummy = 1;
+		if (bpf_map_lookup_elem(&seen_pids, &pid)) {
+			return 0;
+		}
+
+		bpf_map_update_elem(&seen_pids, &pid, &dummy, BPF_ANY);
+
+		struct event_t ev = {
+			.pid = pid,
+			.major = major,
+			.minor = minor,
+		};
+		bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &ev, sizeof(ev));
 	}
 
 	return 0;
