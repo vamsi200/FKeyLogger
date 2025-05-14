360d13439c0cc449ae67d5146deb33a839a97a6b
vamsi200 <vamsivanama891@gmail.com>
2025-05-14 09:33:31 +0530

    added json capability with hidraw and hiddev capability

--- src/main.c
+++ src/main.c
@@ -1,63 +1,88 @@
 #include "test.skel.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdint.h>
 #include <bpf/libbpf.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 struct event_t {
-	uint32_t pid;
-	uint32_t minor;
-	uint32_t major;
+  uint32_t pid;
+  uint32_t minor;
+  uint32_t major;
+  char type[6];
 };
-FILE *fptr;
-static void handle_event(void *ctx, int cpu, void *data, unsigned int data_sz)
-{
-	struct event_t *ev = (struct event_t *)data;
-	fptr = fopen("bpf_output.txt", "a");
-	if (!fptr) {
-		perror("fopen");
-		return;
-	}
-	fprintf(fptr, "%d %d %d\n", ev->pid, ev->minor, ev->major);
-	fclose(fptr);
+
+static FILE *fptr = NULL;
+static volatile int running = 1;
+
+static void handle_event(void *ctx, int cpu, void *data, unsigned int data_sz) {
+  struct event_t *ev = (struct event_t *)data;
+
+  if (!fptr)
+    return;
+
+  fprintf(fptr,
+          "{\"pid\": %u, \"major\": %u, \"minor\": %u, \"type\": \"%s\"}\n",
+          ev->pid, ev->major, ev->minor, ev->type);
 }
 
-int main()
-{
-	struct test_bpf *skel = test_bpf__open_and_load();
-	if (!skel) {
-		fprintf(stderr, "Failed to open and load BPF program\n");
-		return 1;
-	}
+static void handle_signal(int sig) { running = 0; }
 
-	if (test_bpf__attach(skel)) {
-		fprintf(stderr, "Failed to attach BPF program\n");
-		test_bpf__destroy(skel);
-		return 1;
-	}
+int main() {
+  struct test_bpf *skel = test_bpf__open_and_load();
+  if (!skel) {
+    fprintf(stderr, "Failed to open and load BPF program\n");
+    return 1;
+  }
 
-	struct perf_buffer_opts opts = { 0 };
-	opts.sz = sizeof(struct perf_buffer_opts);
+  if (test_bpf__attach(skel)) {
+    fprintf(stderr, "Failed to attach BPF program\n");
+    test_bpf__destroy(skel);
+    return 1;
+  }
 
-	struct perf_buffer *pb = perf_buffer__new(bpf_map__fd(skel->maps.events), 8, handle_event,
-						  NULL, NULL, &opts);
-	if (!pb) {
-		fprintf(stderr, "Failed to create perf buffer\n");
-		test_bpf__destroy(skel);
-		return 1;
-	}
+  fptr = fopen("bpf_output.json", "a");
+  if (!fptr) {
+    perror("fopen");
+    test_bpf__destroy(skel);
+    return 1;
+  }
 
-	printf("Program loaded! Waiting...\n");
+  setvbuf(fptr, NULL, _IOLBF, 0);
 
-	while (1) {
-		int err = perf_buffer__poll(pb, 1000);
-		if (err < 0) {
-			fprintf(stderr, "Error while polling: %d\n", err);
-			break;
-		}
-	}
+  struct perf_buffer_opts opts = {0};
+  opts.sz = sizeof(struct perf_buffer_opts);
 
-	perf_buffer__free(pb);
-	test_bpf__destroy(skel);
-	return 0;
+  struct perf_buffer *pb = perf_buffer__new(bpf_map__fd(skel->maps.events), 8,
+                                            handle_event, NULL, NULL, &opts);
+  if (!pb) {
+    fprintf(stderr, "Failed to create perf buffer\n");
+    fclose(fptr);
+    test_bpf__destroy(skel);
+    return 1;
+  }
+
+  signal(SIGINT, handle_signal);
+  signal(SIGTERM, handle_signal);
+
+  printf("> Program running. Press Ctrl+C to stop.\n");
+
+  while (running) {
+    int err = perf_buffer__poll(pb, 1000);
+    if (err < 0 && running) {
+      fprintf(stderr, "Error while polling: %d\n", err);
+      break;
+    }
+  }
+
+  perf_buffer__free(pb);
+  test_bpf__destroy(skel);
+
+  if (fptr) {
+    fclose(fptr);
+  }
+
+  printf("Exiting...\n");
+  return 0;
 }
